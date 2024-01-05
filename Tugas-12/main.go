aa5305fe7ae719cff034ff3c326cd281578a69dd
muhammad.mirza3107 <muhammad.mirza3107@gmail.com>
2024-01-06 05:09:10 +0700

    tugas-12 revisi

--- Tugas-12/main.go
+++ Tugas-12/main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
-	//soal.Soal1()
-	//soal.Soal2()
-	//soal.Soal3()
+	soal.Soal1()
+	soal.Soal2()
+	soal.Soal3()
 	http.HandleFunc("/soal4", soal.Soal4)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println("http://localhost:8080/soal4")
 }
